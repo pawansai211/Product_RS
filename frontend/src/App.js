5b48f61e9c5329c378eb093022503f119c875866
Pawan Sai <92839649+pawansai211@users.noreply.github.com>
2025-01-28 14:54:17 +0300

    Added Recommendation path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Use BrowserRouter
-import Signup from './components/SignUp';
+import Signup from './components/Signup';
 import Login from './components/Login';
 import Profile from './components/Profile';
 import Upload from './services/UploadData';
 import Home from './components/Home';
+import Recommendations from './components/Recommendations';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/upload" element={<Upload />} />
+        <Route path="/recommendations" element={<Recommendations />} />
       </Routes>
     </Router>
   );
